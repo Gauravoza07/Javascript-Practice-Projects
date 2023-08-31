d71e7c687f7df5ff654b9cb8ccb9bd1e84ad6382
Gauravoza07 <137199562+Gauravoza07@users.noreply.github.com>
2023-08-31 19:09:25 +0000

    done

---
+++ Basics/operation.js
@@ -0,0 +1,38 @@
+let value = 33
+let negValue = -value
+console.log(negValue)
+
+//Arithmetic
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "Gaurav"
+let str2 = "Oza"
+console.log(str1 + str2);
+
+console.log("1" + 2);//Act as string
+console.log(1 + "2");//Act as string
+console.log("1" + 2 + 2);//firt one is string than every input act as string
+console.log(1 + 2 + "2");//firt one is number than every input act as number
+
+console.log(true);
+console.log(true);
+
+let num1;
+num1=5;
+num1++;
+console.log(num1);
+
+//comparision
+let n1 = 5;
+let n2 = 4;
+console.log(n1 < n2);
+console.log(n1 > n2);
+console.log(n1 = n2);
+console.log(n1 >= n2);
+console.log(n1 <= n2);
+console.log(n1 == n2);
+console.log(n1 === n2);
