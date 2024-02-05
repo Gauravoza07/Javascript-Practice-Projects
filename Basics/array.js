172f724191c927f527393a4461e7163000d343c4
Gaurav <ozagaurav03@gmail.com>
2024-02-05 16:50:34 +0530

    initial commit

---
+++ Basics/array.js
@@ -0,0 +1,25 @@
+const arr = ["Gaurav","Mohit","Yash","Karman","Meet","Aakash"]
+
+const myHeros = ["Shaktiman","Spiderman","superman"]
+
+const myArr = new Array(1 , 2 , 3 , 4)
+console.log(arr[0])
+console.log(arr)
+
+//to get length of array
+console.log(myHeros.length)
+
+//to push element inside the array
+myArr.push("Krish")
+
+//pop is used to remove last element from array
+myArr.pop()
+myArr.shift(0)
+
+//shift all element of array add this element at first of array list
+myArr.unshift("Shiva")
+
+//index of array
+console.log(myArr.indexOf("Gaurav"))
+
+//slice ,Splice 
