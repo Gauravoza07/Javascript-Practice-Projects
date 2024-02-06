1203bbe0b79d7c01592ac6770569b47f8ce3593a
Gaurav <ozagaurav03@gmail.com>
2024-02-07 00:29:23 +0530

    initial commit

---
+++ JS_Project/color_changer.js
@@ -0,0 +1,14 @@
+const buttons  = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click',function(e)
+    {
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id="grey"){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+});
