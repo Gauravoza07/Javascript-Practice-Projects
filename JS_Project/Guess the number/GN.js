0a0e185ff1bcb6b53c19a23975d81e7504c36c8c
Gaurav <ozagaurav03@gmail.com>
2024-02-09 00:21:05 +0530

    done

---
+++ JS_Project/Guess the number/GN.js
@@ -0,0 +1,39 @@
+const num = (Math.round(Math.random()*100 + 1));
+
+const submit = document.querySelector('#submit');
+const hint = document.querySelector('.hint');
+const success = document.querySelector('.success-message');
+const userInput = document.querySelector('#guessField');
+
+let nguesses = 1;
+let playGame = true;
+
+
+
+// write validation on user input number
+function validateGuess(userInput){
+    submit.addEventListener
+}
+
+function validateCheck(userInput){
+
+}
+
+function displayMessage(userInput){
+
+}
+
+function displayGuess(nguesses)
+{
+
+}
+
+function newGame()
+{
+
+}
+
+function endGame()
+{
+
+}
