c90f5402d4b79896ccff5aaac8a23b7d581e3fa9
Gaurav <ozagaurav03@gmail.com>
2024-03-03 11:21:37 +0530

    Done

---
+++ Practice/prompt_alert_pract.js
@@ -0,0 +1,14 @@
+// alert gives only alert 
+alert("Hello I am Alert");
+
+//Prompt helps to give pop-up and give input box
+prompt("Hello")
+
+let num  = prompt("Enter a number : ");
+
+if(num % 5 === 0){
+    console.log("Yes!! It is multiple of 5");
+}
+else{
+    console.log("No it is not multiple of 5");
+}
---
+++ Practice/variable_pract.js
@@ -0,0 +1,15 @@
+const product  = {
+    name: "Pen",
+    rating: 5,
+    isDeal:true,
+    price:270,
+}
+
+let profile = {
+    userName : "Rohit Sharma",
+    followers : 569000,
+    following : 4,
+    post : 195,
+    isFollow : false,
+    bio : "Indian Cricketer plays a beautiful pull short",
+}
